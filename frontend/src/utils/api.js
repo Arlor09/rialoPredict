49c6750fd1d752a6b35277b6142c89b6fd4cfa32
Arlor09 <fakeakun11320@gmail.com>
2026-02-02 19:18:10 +0700

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://arlor-stok-prediksi-api.hf.space/api';
 
 // Create axios instance with default config
 const api = axios.create({
